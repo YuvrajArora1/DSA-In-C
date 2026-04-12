855cf4dba654eb19b392eaf9b84a4ccbd2828fda
YuvrajArora06 <arorayuvraj14@gmail.com>
2026-04-12 20:37:24 +0530

    DAY - 45

--- Q45.c
+++ Q45.c
@@ -1,81 +1,78 @@
+/*Problem Statement:
+Find the height (maximum depth) of a given binary tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
-{
-    int data;
-    struct node *left;
-    struct node *right;
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
 };
 
-struct node* newNode(int data)
-{
-    struct node* temp = (struct node*)malloc(sizeof(struct node));
-    temp->data = data;
-    temp->left = NULL;
-    temp->right = NULL;
-    return temp;
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
 
-int height(struct node* root)
-{
-    if(root == NULL)
-        return 0;
+struct TreeNode* buildTree(int* arr, int n) {
+    if (n == 0) return NULL;
 
-    int left = height(root->left);
-    int right = height(root->right);
-
-    if(left > right)
-        return left + 1;
-    else
-        return right + 1;
-}
-
-int main()
-{
-    int n;
-
-    printf("Enter number of nodes: ");
-    scanf("%d",&n);
-
-    int arr[n];
-
-    printf("Enter level order traversal (-1 for NULL): ");
-    for(int i=0;i<n;i++)
-        scanf("%d",&arr[i]);
-
-    if(arr[0] == -1)
-    {
-        printf("Height of tree: 0");
-        return 0;
-    }
-
-    struct node* nodes[n];
-
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i] == -1)
+    struct TreeNode** nodes = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1) {
+            nodes[i] = createNode(arr[i]);
+        } else {
             nodes[i] = NULL;
-        else
-            nodes[i] = newNode(arr[i]);
-    }
-
-    for(int i=0;i<n;i++)
-    {
-        if(nodes[i] != NULL)
-        {
-            int left = 2*i + 1;
-            int right = 2*i + 2;
-
-            if(left < n)
-                nodes[i]->left = nodes[left];
-
-            if(right < n)
-                nodes[i]->right = nodes[right];
         }
     }
 
-    printf("Height of tree: %d", height(nodes[0]));
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+            if (leftIndex < n) {
+                nodes[i]->left = nodes[leftIndex];
+            }
+            if (rightIndex < n) {
+                nodes[i]->right = nodes[rightIndex];
+            }
+        }
+    }
 
+    struct TreeNode* root = nodes[0];
+    free(nodes);
+    return root;
+}
+
+int height(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct TreeNode* root = buildTree(arr, n);
+    printf("%d\n", height(root));
+
+    free(arr);
     return 0;
 }
+
+
+
