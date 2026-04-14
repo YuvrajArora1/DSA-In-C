d9c999908237cc69b3bca4427ff22bfb4f1a09e3
YuvrajArora06 <arorayuvraj14@gmail.com>
2026-04-15 00:50:25 +0530

    DAY - 52

---
+++ Q52.c
@@ -0,0 +1,95 @@
+/*Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)
+- Third line contains two node values
+
+Output Format:
+- Print the LCA value*/
+
+#include <stdio.h>
+#include <stdlib.h>
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct TreeNode* buildTree(int* arr, int n) {
+    if (n == 0) return NULL;
+
+    struct TreeNode** nodes = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1) {
+            nodes[i] = createNode(arr[i]);
+        } else {
+            nodes[i] = NULL;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+            if (leftIndex < n) {
+                nodes[i]->left = nodes[leftIndex];
+            }
+            if (rightIndex < n) {
+                nodes[i]->right = nodes[rightIndex];
+            }
+        }
+    }
+
+    struct TreeNode* root = nodes[0];
+    free(nodes);
+    return root;
+}
+
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (root == NULL) return NULL;
+
+    if (root->val > p->val && root->val > q->val) {
+        return lowestCommonAncestor(root->left, p, q);
+    }
+    if (root->val < p->val && root->val < q->val) {
+        return lowestCommonAncestor(root->right, p, q);
+    }
+    return root;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    struct TreeNode* root = buildTree(arr, n);
+    free(arr);
+
+    int pVal, qVal;
+    scanf("%d %d", &pVal, &qVal);
+    struct TreeNode* p = createNode(pVal);
+    struct TreeNode* q = createNode(qVal);
+
+    struct TreeNode* lca = lowestCommonAncestor(root, p, q);
+    if (lca != NULL) {
+        printf("%d\n", lca->val);
+    } else {
+        printf("LCA does not exist.\n");
+    }
+
+    return 0;
+}
+
